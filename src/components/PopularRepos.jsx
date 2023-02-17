d4ca763c3640ce358c2a4180dcafcd311cc94f37
Pablo Cigoy <pablocigoy@gmail.com>
2023-02-17 14:50:37 +0100

    upload

--- src/components/PopularRepos.jsx
+++ src/components/PopularRepos.jsx
@@ -56,13 +56,14 @@ export const PopularRepos = ({ dataAPI }) => {
 
           {dataAPI.items &&
             dataAPI.items.map((oneRepo) => {
+              const urlGithub = oneRepo.git_url.replace('verkstedt-cigoy.netlify.app/', '').replace('git://', '').replace('.git', '')
               return (
                 <tbody key={oneRepo.id}>
                   <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 '>
                     <td className='px-3 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border border-slate-700'>{oneRepo.name}</td>
                     <td className='px-2 py-4 font-medium  whitespace-nowrap dark:text-blue-500 hover:underline border border-slate-700'>
-                      <a target='_blank' rel='noreferrer' href={oneRepo.git_url.replace('verkstedt-cigoy.netlify.app/', '').replace('git://', '').replace('.git', '')}>
-                        {oneRepo.git_url.replace('git://', '').replace('.git', '')}
+                      <a target='_blank' rel='noreferrer' href={urlGithub}>
+                        {urlGithub}{' '}
                       </a>
                     </td>
                     <td className='px-2 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border border-slate-700'>{oneRepo.description}</td>
@@ -87,9 +88,7 @@ export const PopularRepos = ({ dataAPI }) => {
             <th className='border-spacing-x-2 p-3 border-separate '>github URL</th>
             <th className='border-spacing-x-2 border-separate px-18 '>Description</th>
             <th className='border-spacing-x-2 border-separate pr-10 '>Stars</th>
-            <th  className='px-6 py-3'>
-              Action
-            </th>
+            <th className='px-6 py-3'>Action</th>
           </tr>
         </thead>
 
